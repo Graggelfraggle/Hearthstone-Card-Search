d0b884d6395eb9444111aae07b48f40b3a03c8ea
Graggelfraggle <gragg@hotmail.co.uk>
2017-10-15 12:35:49 +0100

    Initial commit

---
+++ CardSearch.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import TextInput from './TextInput';
+
+class CardSearch extends Component {
+
+	
+
+	constructor(){
+      super();
+      this.state={
+        cards:[],
+        searchTerm:'argent',
+      };
+    }
+
+	
+
+    componentDidMount() {
+		axios.get('https://omgvamp-hearthstone-v1.p.mashape.com/cards/search/'+this.props.searchTerm+'?callback=&collectible=&locale=', {
+      headers: { "X-Mashape-Key": 'zRiX3SbUS7mshgH8qDcn38YscFQPp11Mh2cjsnEVcKla76V7OL' }
+    })
+		.then(res => {
+
+	    const cards= res.data; 
+	    this.setState({cards});
+	    console.log(res.data)
+	  });
+  	}
+
+  	ComponentWillReceiveProps(nextProps){
+  		axios.get('https://omgvamp-hearthstone-v1.p.mashape.com/cards/search/'+nextProps.searchTerm+'?callback=&collectible=&locale=', {
+      headers: { "X-Mashape-Key": 'zRiX3SbUS7mshgH8qDcn38YscFQPp11Mh2cjsnEVcKla76V7OL' }
+    })
+		.then(res => {
+
+	    const cards= res.data; 
+	    this.setState({cards});
+	    console.log(res.data)
+	  });
+  	}
+
+  	
+
+	render() {
+		{this.props.searchTerm}
+
+
+		var set = [0, 'Naxxramas', 'Goblins vs Gnomes','Blackrock Mountain','The Grand Tournament', 'The League of Explorers','Whispers of the Old Gods', 'One Night in Karazhan', 'Mean Streets of Gadgetzan', "Journey to Un'Goro", 'Knights of the frozen Throne']
+		var cards = this.state.cards
+		var list = [];
+		var number = Object.keys(cards).length;
+		var cardClass = [0,'Druid','Hunter','Mage','Paladin','Priest','Rogue','Shaman','Warlock','Warrior']
+		var rarity = [0,'Basic','Common','Rare','Epic','Legendary']
+
+		if (this.props.cardSet!=0){	
+			for (var i=0;i<number;i++) { 			
+				if (this.state.cards[i].cardSet===set[this.props.cardSet]){
+					list.push(<div>{this.state.cards[i].name}</div>)
+
+				} 
+			}
+		} else if (this.props.cardClass!=0){
+			for (var i=0;i<number;i++) { 			
+				if (this.state.cards[i].playerClass===cardClass[this.props.cardClass]){
+					list.push(<div>{this.state.cards[i].name}</div>)
+
+				}
+			}
+		} else if (this.props.cardRarity!=0){
+			for (var i=0;i<number;i++) { 			
+				if (this.state.cards[i].rarity===rarity[this.props.cardRarity]){
+					list.push(<div>{this.state.cards[i].name}</div>)
+
+				}
+			}
+		} else {
+			for (var i=0;i<number;i++) { 			
+				list.push(<div>{this.state.cards[i].name}</div>)
+			}
+		}
+
+
+
+		return (
+			<div>
+				{set[this.props.cardSet]}
+				{cardClass[this.props.cardClass]}
+				{rarity[this.props.cardRarity]}
+				{list}
+				{this.props.searchTerm}
+			
+				
+				
+
+			</div>
+		);
+
+		
+	}
+};
+
+export default CardSearch
---
+++ CategorySelector.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+import './categoryselector.css';
+import CardSearch from './CardSearch.js';
+
+class CategorySelector extends Component {
+
+	constructor(){
+      super();
+      this.state={
+        setSelection:0,
+        setClass:0,
+        setRarity:0,
+        searchTerm:'argent',
+      };
+      this.onSetSelection=this.onSetSelection.bind(this)
+      this.onClassSelection=this.onClassSelection.bind(this)
+      this.onRaritySelection=this.onRaritySelection.bind(this)
+      this.onInput=this.onInput.bind(this)
+      this.handleInput=this.handleInput.bind(this)
+    }
+
+
+	onSetSelection(event) {
+		if(this.state.setSelection != event.target.dataset.set){
+			this.setState({ setSelection: event.target.dataset.set});
+			this.setState({ setRarity: 0});
+			this.setState({ setClass: 0});
+		}else{
+			this.setState({ setSelection: 0});
+		}
+		
+	}
+	onClassSelection(event) {
+		if(this.state.setClass != event.target.dataset.class){
+			this.setState({ setClass: event.target.dataset.class});
+			this.setState({ setSelection: 0});
+			this.setState({ setRarity: 0});
+		}else{
+			this.setState({ setClass: 0});
+		}
+	}
+	onRaritySelection(event) {
+		if(this.state.setRarity != event.target.dataset.rarity){
+			this.setState({ setRarity: event.target.dataset.rarity});
+			this.setState({ setSelection: 0});
+			this.setState({ setClass: 0});
+		}else{
+			this.setState({ setRarity: 0});
+		}
+	}
+
+	handleInput(text){
+    this.props.onChange(text);
+    
+  }
+
+  onInput(e){
+      console.log(e.target.value)
+      this.setState({searchTerm:e.target.value});
+      this.handleInput(e.target.value);
+  }
+
+	render(){
+
+		
+
+		return(
+		<div>	
+			<div>{this.state.setSelection}</div>
+			<div>{this.state.setClass}</div>
+			<div>{this.state.setRarity}</div>
+			<div>{this.state.searchTerm}</div>
+	        <div onClick={this.props.cardSetClick} className ="Choices">Card Set</div>
+	          <div onClick={this.onSetSelection} data-set ='1' className={this.props.cardSet}>Naxx</div>
+	          <div onClick={this.onSetSelection} data-set ='2' className={this.props.cardSet}>GvG</div>
+	          <div onClick={this.onSetSelection} data-set ='3' className={this.props.cardSet}>BRM</div>
+	          <div onClick={this.onSetSelection} data-set ='4' className={this.props.cardSet}>TGT</div>
+	          <div onClick={this.onSetSelection} data-set ='5' className={this.props.cardSet}>LoE</div>
+	          <div onClick={this.onSetSelection} data-set ='6' className={this.props.cardSet}>WOG</div>
+	          <div onClick={this.onSetSelection} data-set ='7' className={this.props.cardSet}>Kara</div>
+	          <div onClick={this.onSetSelection} data-set ='8' className={this.props.cardSet}>MSG</div>
+	          <div onClick={this.onSetSelection} data-set ='9' className={this.props.cardSet}>Un'goro</div>
+	          <div onClick={this.onSetSelection} data-set ='10' className={this.props.cardSet}>KFT</div>
+	        <div onClick={this.props.cardClassClick} className ="Choices">Class</div>
+	        	<div onClick={this.onClassSelection} data-class ='1' className={this.props.cardClass}>Druid</div>
+	        	<div onClick={this.onClassSelection} data-class ='2' className={this.props.cardClass}>Hunter</div>
+	        	<div onClick={this.onClassSelection} data-class ='3'className={this.props.cardClass}>Mage</div>
+	        	<div onClick={this.onClassSelection} data-class ='4' className={this.props.cardClass}>Paladin</div>
+	        	<div onClick={this.onClassSelection} data-class ='5' className={this.props.cardClass}>Priest</div>
+	        	<div onClick={this.onClassSelection} data-class ='6' className={this.props.cardClass}>Rogue</div>
+	        	<div onClick={this.onClassSelection} data-class ='7' className={this.props.cardClass}>Shaman</div>
+	        	<div onClick={this.onClassSelection} data-class ='8' className={this.props.cardClass}>Warlock</div>
+	        	<div onClick={this.onClassSelection} data-class ='9' className={this.props.cardClass}>Warrior</div>
+	        <div onClick={this.props.cardRarityClick} className ="Choices">Card Rarity</div>
+	        	<div onClick={this.onRaritySelection} data-rarity ='1' className={this.props.cardRarity}>Basic</div>
+	        	<div onClick={this.onRaritySelection} data-rarity ='2' className={this.props.cardRarity}>Common</div>
+	        	<div onClick={this.onRaritySelection} data-rarity ='3' className={this.props.cardRarity}>Rare</div>
+	        	<div onClick={this.onRaritySelection} data-rarity ='4' className={this.props.cardRarity}>Epic</div>
+	        	<div onClick={this.onRaritySelection} data-rarity ='5' className={this.props.cardRarity}>Legendary</div>
+	    	<CardSearch searchTerm={this.state.searchTerm} cardSet={this.state.setSelection} cardClass={this.state.setClass} cardRarity={this.state.setRarity}/>
+	    	<form className="reactForm" onChange={this.onInput}>
+            	<input type='text' />
+            </form>
+	    </div>
+
+		)		
+	}
+}
+
+export default CategorySelector;
+
+/*
+
+https://stackoverflow.com/questions/28511207/react-js-onclick-event-handler
+
+Use method here to pass selection choice
+
+Then add search option and you're basically done
+
+
+*/
